3c033453aef27f2bb79880646d657277025596f3
mxmo0rhuhn <max-schrimpf@hotmail.com>
2014-03-26 01:29:02 +0100

    correced file header

--- src/main/java/org/sonar/plugins/fileparser/batch/FileParserSensor.java
+++ src/main/java/org/sonar/plugins/fileparser/batch/FileParserSensor.java
@@ -1,6 +1,7 @@
 /*
  * SonarQube File Parser Plugin
  * Copyright (C) 2014 Max Schrimpf
+ * dev@sonar.codehaus.org
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
